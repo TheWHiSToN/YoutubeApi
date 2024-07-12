760c7465dd2af446b3a467912fe7af5b438972cd
Ali Aktaş <muyu45@gmail.com>
2024-07-12 15:14:52 +0300

    Unitofworks yapısı

--- Infrastructure/YoutubeApi.Persistence/Registration.cs
+++ Infrastructure/YoutubeApi.Persistence/Registration.cs
@@ -7,8 +7,10 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using YoutubeApi.Application.Interfaces.Repositories;
+using YoutubeApi.Application.Interfaces.UnitOfWorks;
 using YoutubeApi.Persistence.Context;
 using YoutubeApi.Persistence.Repositories;
+using YoutubeApi.Persistence.UnitOfWorks;
 
 namespace YoutubeApi.Persistence
 {
@@ -19,6 +21,7 @@ namespace YoutubeApi.Persistence
             services.AddDbContext<AppDbContext>(opt => opt.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
             services.AddScoped(typeof(IReadRepository<>),typeof(ReadRepository<>));
             services.AddScoped(typeof(IWriteRepository<>), typeof(WriteRepository<>));
+            services.AddScoped<IUnitOfWork,UnitOfWork>();
         }
     }
 }
