3c6a8d643b15997b7fde3f94797900b29c32d0ea
Ali Aktaş <muyu45@gmail.com>
2024-07-23 18:39:55 +0300

    automapper yapısı geliştirildi

--- Presentation/YoutubeApi.Api/Program.cs
+++ Presentation/YoutubeApi.Api/Program.cs
@@ -1,5 +1,7 @@
 using YoutubeApi.Persistence;
 using YoutubeApi.Application;
+using YoutubeApi.Mapper;
+
 
 
 var builder = WebApplication.CreateBuilder(args);
@@ -19,6 +21,7 @@ builder.Configuration
 
 builder.Services.AddPersistence(builder.Configuration);
 builder.Services.AddApplication();
+builder.Services.AddCustomMapper();
 
 var app = builder.Build();
 
