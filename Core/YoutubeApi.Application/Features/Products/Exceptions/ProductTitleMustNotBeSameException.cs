a50c5ecfcd47ecc4ad88cbdc4c302690d2fdfff2
Ali Aktaş <muyu45@gmail.com>
2024-07-25 15:11:45 +0300

    Project Rules tamamlandı.

---
+++ Core/YoutubeApi.Application/Features/Products/Exceptions/ProductTitleMustNotBeSameException.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using YoutubeApi.Application.Bases;
+
+namespace YoutubeApi.Application.Features.Products.Exceptions
+{
+    public class ProductTitleMustNotBeSameException : BasesExceptions
+    {
+        public ProductTitleMustNotBeSameException() : base("Ürün başlığı zaten var!") { }
+        
+    }
+}
