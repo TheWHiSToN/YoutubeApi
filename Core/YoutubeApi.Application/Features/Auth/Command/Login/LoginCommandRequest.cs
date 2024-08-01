3670e9d735758edd364ff7dffb395f71288d40dd
Ali Aktaş <muyu45@gmail.com>
2024-08-01 14:52:28 +0300

    Mediatr ve JWT ile Login İşlemleri tamamlandı.

---
+++ Core/YoutubeApi.Application/Features/Auth/Command/Login/LoginCommandRequest.cs
@@ -0,0 +1,18 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YoutubeApi.Application.Features.Auth.Command.Login
+{
+    public class LoginCommandRequest :IRequest<LoginCommandResponse>
+    {
+        [DefaultValue("aliaktas12@gmail.com")]
+        public string Email { get; set; }
+        [DefaultValue("123456")]
+        public string Password { get; set; }
+    }
+}
